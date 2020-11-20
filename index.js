f8468e66f96221e4bf6c94b81f29ee821369a4e8
Jon Owens <jonowens84@gmail.com>
2020-11-20 11:59:38 +0000

    first array method descriptions

---
+++ index.js
@@ -0,0 +1,31 @@
+//for this example each element are a string (or a name) in an array
+let avengers = ['Thor','Ironman','Doctor Strange','Captain Marvel', 'Black Widow','Spiderman']
+//.pop 
+
+
+avengers.pop;
+//.pop method allows you to remove the last item from the end of an array
+//.pop spiderman is removed from the end of the array and returned as a single item
+returns 'Spiderman'
+// the original avengers array is mutated and now has a new length.
+avengers = ['Thor','Ironman','Doctor Strange','Captain Marvel', 'Black Widow']
+
+
+// .push 
+
+avengers = ['Thor','Ironman','Doctor Strange','Captain Marvel', 'Black Widow']
+// .push method allows you to add a new item to the end of an array
+avengers.push('Spiderman')
+
+
+//.shift
+
+avengers.shift;
+
+//.shift method removes te first item in the array.
+//.shift Thor is removed from the start of the array and returned as a sngle item.
+returns 'Thor'
+//the original avengers array is mutated and now has a new length.
+let avengers = ['Ironman','Doctor Strange','Captain Marvel', 'Black Widow','Spiderman']
+
+//.find
